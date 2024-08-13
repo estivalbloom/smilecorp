b19f0c173801b0de34a847142c5f9eac15a9d4b3
estivalbloom <cogmonocle@gmail.com>
2024-08-13 19:22:33 -0400

    Initialize vue and deploy action

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<header>
+	</header>
+
+	<RouterView />
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+  
+<style scoped>
+header {
+	line-height: 1.5;
+	max-height: 100vh;
+}
+</style>
